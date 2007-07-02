841198ae3a8d6b9756fa48f66a34536bf6712724
fredlin <none@none>
2007-07-02 05:37:17 +0000

    enable to use pylons and webhelper in tg project
    --HG--
    extra : convert_revision : svn%3A77541ad4-5f01-0410-9ede-a1b63cd9a898/trunk%403201

--- tg/pastetemplate.py
+++ tg/pastetemplate.py
@@ -4,7 +4,7 @@ Definitions for TurboGears quickstart templates
 from paste.script import templates
 
 class TurboGearsTemplate(templates.Template):
-    egg_plugins = []
+    egg_plugins = ['TurboGears2', 'Pylons', 'WebHelpers']
     required_templates = []
     _template_dir = 'pastetemplates/turbogears'
     summary = 'TurboGears 2.0 Template'
