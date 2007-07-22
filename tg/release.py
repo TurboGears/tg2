90bc53c743095fd3a6a848aa2447b6ae6ab1a98f
carndt <none@none>
2007-07-22 18:21:11 +0000

    Convert description in setup.py to rest format for better display in Cheeseshop
    --HG--
    extra : convert_revision : svn%3A77541ad4-5f01-0410-9ede-a1b63cd9a898/trunk%403340

--- tg/release.py
+++ tg/release.py
@@ -2,23 +2,25 @@
 version = "2.0a1"
 description = "Next generation TurboGears built on Pylons"
 long_description="""
-TurboGears brings together a variety of best of breed python tools 
-to create an easy to install, easy to use web megaframework. 
+TurboGears brings together a variety of best of breed python tools
+to create an easy to install, easy to use web megaframework.
 
-It provides and integrated and well tested set of tools for 
-everything you need to build dynamic, database driven applications.  
-TurboGears 2 provides you with everything you need from the 
-javascript programming tools, like ToscaWidgets and automatic 
-JSON generation, to one of the world's most powerful Object 
+It provides and integrated and well tested set of tools for
+everything you need to build dynamic, database driven applications.
+TurboGears 2 provides you with everything you need from the
+javascript programming tools, like ToscaWidgets and automatic
+JSON generation, to one of the world's most powerful Object
 Relational Mappers (SQLAlchemy).
 
+The latest development version is available in the
+`TurboGears subversion repository`_.
 
-The latest development version is available in
-<a href="http://svn.turbogears.org/trunk#egg=turbogears-dev"
->the TurboGears subversion repository</a>."""
+.. _TurboGears subversion repository:
+    http://svn.turbogears.org/trunk#egg=turbogears-dev
+"""
 url="http://www.turbogears.org"
 author='Alberto Valverde, Mark Ramm'
 email = "alberto@toscat.net, mark.ramm@gmail.com"
-copyright = """Copyright 2005-2007 Kevin Dangoor, 
-Alberto Valverde, Mark.Ramm and contributors"""
+copyright = """Copyright 2005-2007 Kevin Dangoor,
+Alberto Valverde, Mark Ramm and contributors"""
 license = "MIT"
