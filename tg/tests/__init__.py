10c89ca63c673f349a998e100049be681598e95d
max <none@none>
2008-01-12 12:24:44 +0000

    wrote a create_request() functions which works similarly to testutil.createRequest from TG1
    --HG--
    extra : convert_revision : svn%3A77541ad4-5f01-0410-9ede-a1b63cd9a898/trunk%403874

--- tg/tests/__init__.py
+++ tg/tests/__init__.py
@@ -4,6 +4,9 @@ import os
 from unittest import TestCase
 from xmlrpclib import loads, dumps
 
+import webob
+import pylons
+from paste.registry import Registry
 from paste.registry import RegistryManager
 from paste.fixture import TestApp
 from paste.wsgiwrappers import WSGIRequest, WSGIResponse
@@ -36,6 +39,33 @@ def make_app(controller_klass=None, environ=None):
     app = RegistryManager(app)
     app = httpexceptions.make_middleware(app)
     return TestApp(app)
+
+def create_request(path, environ=None):
+    """Helper used in test cases to quickly setup a request obj.
+
+    ``path``
+        The path will become PATH_INFO
+    ``environ``
+        Additional environment
+
+    Returns an instance of the `webob.Request` object.
+    """
+    # setup the environ
+    extra_environ = {'pylons.routes_dict':dict(action='index'),
+                    'paste.config':dict(global_conf=dict(debug=True))}
+    if environ is None:
+        environ = {}
+    environ.update(extra_environ)
+    # create a "blank" WebOb Request object
+    req = webob.Request.blank(path, environ)
+    # setup a Registry
+    reg = environ.setdefault('paste.registry', Registry())
+    reg.prepare()
+    # setup pylons.request to point to our Registry
+    reg.register(pylons.request, req)
+    # setup tmpl context
+    context._push_object(ContextObj())
+    return req
 
 class TestWSGIController(TestCase):
     def setUp(self):
---
+++ tg/tests/test_controllers.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import tg
+from tg.controllers import *
+from tg.tests import TestWSGIController, make_app, create_request
+from nose.tools import eq_
+
+def test_create_request():
+    environ = { 'SCRIPT_NAME' : '/xxx' }
+    request = create_request('/', environ)
+    eq_('http://localhost/xxx/hello', tg.request.relative_url('hello'))
