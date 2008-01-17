4d0f9ebe487bbe517c4ea2290ce1fd524b2dc368
fredlin <none@none>
2008-01-17 10:29:34 +0000

    update tg concept
    --HG--
    extra : convert_revision : svn%3A77541ad4-5f01-0410-9ede-a1b63cd9a898/trunk%403965

--- tg/__init__.py
+++ tg/__init__.py
@@ -1,41 +1,15 @@
 """
-TurboGears 2 is a reinvention of TurboGears and a return to TurboGears' roots.
+TurboGears 2 is a reinvention of TurboGears and a return to TurboGears' roots.TurboGears is a project that is built upon a foundation of library development best-of-breed component selection, and the re-use of already existing code. And it was always intended to be a small collection of tools, docs, and helpers that made developing with that best-of-breed stack easy.In retrospect, some of the code that was added to the main TurboGears project should have been released as independent projects that integrate with TurboGears. This would have allowed those pieces to grow independently of TurboGears, and would have allowed TurboGears to remain smaller and easier to develop and debug.TurboGears 0.5 release was just a few hundred lines of Python code, but it built on thousands of lines of code in other libraries. Those libraries had alreay been deployed, used, and tested, and were known to be "production ready."TurboGears2 returns to that philosophy. It is built on Pylons, but it brings a best-of-breed approach to Pylons. TurboGears 2 is commited to the following Python components and libraries, which are backwards compatable with TurboGears 1.1:
 
-TurboGears is a project that is built upon a foundation of reuse and building up.
-In retrospect, much of the code that was home grown in the TurboGears project
-should have been released as independent projects that integrate with TurboGears.
-This would have allowed better growth of those pieces.
-
-TurboGears 0.5 release was just a few hundred lines of Python code that
-built upon thousands of lines of other libraries.
-
-Now TurboGears2 is built on Pylons and allows you to use
-your favorite Python components and libraries::
-
-    * Models: SQLAlchemy, SQLObject, or plain old DB-API.
-    * Template engines: Genshi, Kid, Myghty, Mako, Cheetah, or
-      whatever you like - using Buffet.
-    * URL Dispatching: Object dispatch, Routes, or plug in your favorite.
-    * AJAX: ToscaWidgets, WebHelpers based on Prototype or Mochikit, Dojo,
-      JQuery, Ext & more.
-
+    * Models: SQLAlchemy    * Template engines: Genshi    * URL Dispatching: Object dispatch    * Form Handling: ToscaWidgets
+    
 The zen of TurboGears is::
 
     Keep simple things simple and complex things possible
     Give good defaults everywhere and allow choices where they matter
-    Don't pretend that there's one obvious way, when there isn't
+    Do your best to do things the right way,    But when there's no "one right way," don't pretend there is.
 
-TurboGears 2 is to Pylons as Ubuntu is to Debian.
-
-We're focused on user experience, and creating a user friendly environment.
-We make choices, setup sane defaults, and build on top of a powerfull,
-flexible core.
-Pylons provide that core, and focuses on maximizing flexibility, stability.
-
-We don't intend to hide that power and flexibility from advanced users,
-we just want them to be able to get up and running quickly, and have a
-well lit development path to walk.
-
+Mark Ramm described the relationship between TurboGears and Pylons this way "TurboGears 2 is to Pylons as Ubuntu is to Debian."In other words we're focused on user experience, and creating a novice-friendly environment. We ship a smaller subset of components, and thus are better able to focus, test, and document things so that new users have the best possible experience.Meanwhile Pylons provides the power and flexibility of the underlying core.And like Ubuntu, we don't intend to hide that power and flexibility from advanced users, but we know that they want things set up to just work too.Sensible defaults actually encourage code re-use within TurboGears because they make it possible for a group of TurboGears components to share assumptions about how things will work.
 """
 from tg.controllers import TurboGearsController, redirect, url
 from tg.flash import flash, get_flash, get_status
@@ -47,7 +21,6 @@ from pylons import c as context
 from pylons import g as app_globals
 from pylons import request
 from pylons import session
-
 
 
 class TurboGearsApplication(PylonsApp):
