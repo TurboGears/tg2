83beee9ce5b5fb040e442290e3235e6293cdcd1b
percious <none@none>
2008-02-24 05:12:13 +0000

    forgot to add base.py in
    --HG--
    extra : convert_revision : svn%3A77541ad4-5f01-0410-9ede-a1b63cd9a898/trunk%404144

---
+++ tg/tests/base.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+
+import os, shutil
+from unittest import TestCase
+from xmlrpclib import loads, dumps
+
+import webob
+import beaker
+import pylons
+from paste.registry import Registry
+from paste.registry import RegistryManager
+from paste.fixture import TestApp
+from paste.wsgiwrappers import WSGIRequest, WSGIResponse
+from paste import httpexceptions
+
+from tg import context
+from pylons.util import ContextObj, PylonsContext
+from pylons.controllers.util import Request, Response
+from tg.controllers import TurboGearsController
+from pylons.testutil import ControllerWrap, SetupCacheGlobal
+#import pylons.tests
+
+from beaker.middleware import CacheMiddleware
+
+data_dir = os.path.dirname(os.path.abspath(__file__))
+session_dir = os.path.join(data_dir, 'session')
+
+def setup_session_dir():
+    if not os.path.exists(session_dir):
+        os.makedirs(session_dir)
+    
+def teardown_session_dir():
+    shutil.rmtree(session_dir, ignore_errors=True)
+
+
+default_environ = {
+    'pylons.use_webob' : False,
+    'pylons.routes_dict': dict(action='index'),
+    'paste.config': dict(global_conf=dict(debug=True))
+}
+
+def make_app(controller_klass=None, environ=None):
+    """Creates a `TestApp` instance."""
+    if environ is None:
+        environ = {}
+    environ['pylons.routes_dict'] = {}
+    environ['pylons.routes_dict']['action'] = "route"
+
+    if controller_klass is None:
+        controller_klass = TGController
+
+    app = ControllerWrap(controller_klass)
+    app = SetupCacheGlobal(app, environ, setup_cache=True, setup_session=True)    
+    app = RegistryManager(app)
+    app = beaker.middleware.SessionMiddleware(app, {}, data_dir=session_dir)
+    app = CacheMiddleware(app, {}, data_dir=os.path.join(data_dir, 'cache'))
+    app = httpexceptions.make_middleware(app)
+    return TestApp(app)
+
+
+def create_request(path, environ=None):
+    """Helper used in test cases to quickly setup a request obj.
+
+    ``path``
+        The path will become PATH_INFO
+    ``environ``
+        Additional environment
+
+    Returns an instance of the `webob.Request` object.
+    """
+    # setup the environ
+    if environ is None:
+        environ = {}
+    environ.update(default_environ)
+    # create a "blank" WebOb Request object
+    # using Pylon's Request which is a webob Request plus
+    # some compatibility methods
+    req = Request.blank(path, environ)
+    # setup a Registry
+    reg = environ.setdefault('paste.registry', Registry())
+    reg.prepare()
+    # setup pylons.request to point to our Registry
+    reg.register(pylons.request, req)
+    # setup tmpl context
+    context._push_object(ContextObj())
+    return req
+
+class TestWSGIController(TestCase):
+    def setUp(self):
+        context._push_object(ContextObj())
+
+    def tearDown(self):
+        context._pop_object()
+        
+    def get_response(self, **kargs):
+        url = kargs.pop('_url', '/')
+        self.environ['pylons.routes_dict'].update(kargs)
+        return self.app.get(url, extra_environ=self.environ)
+
+    def post_response(self, **kargs):
+        url = kargs.pop('_url', '/')
+        self.environ['pylons.routes_dict'].update(kargs)
+        return self.app.post(url, extra_environ=self.environ, params=kargs)
