c179380750a2924888e0de52f8312d5906e23c27
mramm <none@none>
2008-04-02 02:42:55 +0000

    Updated validation to fix dict validator bug.
    --HG--
    extra : convert_revision : svn%3A77541ad4-5f01-0410-9ede-a1b63cd9a898/trunk%404325

--- tg/controllers.py
+++ tg/controllers.py
@@ -36,6 +36,7 @@ class DecoratedController(WSGIController):
         new_params = None
         errors = {}
         if isinstance(validation.validators, dict):
+            new_params = {}
             for field, validator in validation.validators.iteritems():
                 try:
                     validator.to_python(params.get(field))
