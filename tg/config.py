d06c6ef6bb9b380a6325b74b9ebbc420bd2eaa23
mramm <none@none>
2008-06-17 20:32:26 +0000

    A couple of config helper classes.
    --HG--
    extra : convert_revision : svn%3A77541ad4-5f01-0410-9ede-a1b63cd9a898/trunk%404759

---
+++ tg/config.py
@@ -0,0 +1,38 @@
+"""Simple AppSetup helper class"""
+
+class Bunch(dict):
+    """A dictionary that provides attribute-style access."""
+
+    def __getitem__(self, key):
+        item = dict.__getitem__(self, key)
+        if callable(item):
+            return item()
+        return item
+
+    def __getattr__(self, name):
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError(name)
+
+    __setattr__ = dict.__setitem__
+
+    def __delattr__(self, name):
+        try:
+            del self[name]
+        except KeyError:
+            raise AttributeError(name)
+
+class AppSetup(Bunch):
+    """Class to store application configuration
+    
+    This class should have configuration/setup information 
+    that is NESSISARY for proper application function.  
+    Deployment specific configuration information should go in 
+    the config files (eg: development.ini or production.ini." 
+    """
+    
+    def __init__(self):
+        self.stand_alone = True
+    
+    pass
