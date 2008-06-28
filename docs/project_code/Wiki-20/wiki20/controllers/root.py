4a48d98770ef8444911580c9fc40c6db85016114
brunojm <none@none>
2008-06-28 21:36:35 +0000

    change root.py to another part of tutorial
    --HG--
    extra : convert_revision : svn%3A77541ad4-5f01-0410-9ede-a1b63cd9a898/trunk%404832

--- docs/project_code/Wiki-20/wiki20/controllers/root.py
+++ docs/project_code/Wiki-20/wiki20/controllers/root.py
@@ -3,16 +3,18 @@ from wiki20.lib.base import BaseController
 from tg import expose, flash
 from pylons.i18n import ugettext as _
 #from tg import redirect, validate
-#from wiki20.model import DBSession, metadata
+from wiki20.model import DBSession, metadata
 #from dbsprockets.dbmechanic.frameworks.tg2 import DBMechanic
 #from dbsprockets.saprovider import SAProvider
+from wiki20.model.page import Page
 
 class RootController(BaseController):
     #admin = DBMechanic(SAProvider(metadata), '/admin')
 
-    @expose('wiki20.templates.index')
-    def index(self):
-        return dict(page='index')
+    @expose('wiki20.templates.page')
+    def index(self, pagename="FrontPage"):
+        page = DBSession.query(Page).filter_by(pagename=pagename).one()
+        return dict(wikipage=page)
 
     @expose('wiki20.templates.about')
     def about(self):
