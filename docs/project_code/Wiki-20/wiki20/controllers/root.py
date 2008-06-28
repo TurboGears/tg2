ef5dfc8ea8ab95f168aafad0d6b33ceff95f9546
brunojm <none@none>
2008-06-28 21:54:27 +0000

    adding save method
    --HG--
    extra : convert_revision : svn%3A77541ad4-5f01-0410-9ede-a1b63cd9a898/trunk%404834

--- docs/project_code/Wiki-20/wiki20/controllers/root.py
+++ docs/project_code/Wiki-20/wiki20/controllers/root.py
@@ -2,7 +2,7 @@
 from wiki20.lib.base import BaseController
 from tg import expose, flash
 from pylons.i18n import ugettext as _
-#from tg import redirect, validate
+from tg import redirect, validate
 from wiki20.model import DBSession, metadata
 #from dbsprockets.dbmechanic.frameworks.tg2 import DBMechanic
 #from dbsprockets.saprovider import SAProvider
@@ -12,7 +12,7 @@ class RootController(BaseController):
     #admin = DBMechanic(SAProvider(metadata), '/admin')
 
     @expose('wiki20.templates.page')
-    def index(self, pagename="FrontPage"):
+    def default(self, pagename="FrontPage"):
         page = DBSession.query(Page).filter_by(pagename=pagename).one()
         return dict(wikipage=page)
 
@@ -24,3 +24,10 @@ class RootController(BaseController):
     @expose('wiki20.templates.about')
     def about(self):
         return dict()
+
+    @expose()
+    def save(self, pagename, data, submit):
+        page = DBSession.query(Page).filter_by(pagename=pagename).one()
+        page.data = data
+        DBSession.commit() # Tells database to commit changes permanently
+        redirect("/" + pagename)
