88e86f50aa1a892ea6c892a8a74f06c34a120e43
mramm <none@none>
2008-06-29 17:46:09 +0000

    Removing paver dependency temporarily.
    --HG--
    extra : convert_revision : svn%3A77541ad4-5f01-0410-9ede-a1b63cd9a898/trunk%404845

--- setup.py
+++ setup.py
@@ -1,7 +1,38 @@
 import os
-if os.path.exists("paver-minilib.zip"):
-    import sys
-    sys.path.insert(0, "paver-minilib.zip")
+here = os.path.abspath(os.path.dirname(__file__))
+execfile(os.path.join(here, 'tg', 'release.py'))
+from setuptools import find_packages
+from setuptools import setup
 
-import paver.command
-paver.command.main()
+setup(
+    name='TurboGears2',
+    version=version,
+    description=description,
+    long_description=long_description,
+    classifiers=[],
+    keywords='turbogears pylons',
+    author=author,
+    author_email=email,
+    url=url,
+    license=license,
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Babel',
+        'Pylons',
+        'Genshi>=0.4',
+        'SQLAlchemy>=0.4',
+        'ToscaWidgets>=0.9.2',
+    ],
+    extras_require={
+        'core-testing':["nose", "TurboKid", "TurboJson"]
+    },
+    entry_points='''
+        [paste.global_paster_command]
+        tginfo = tg.commands.info:InfoCommand
+        [turbogears2.command]
+        serve = paste.script.serve:ServeCommand [Config]
+        shell = pylons.commands:ShellCommand
+    '''
+)
