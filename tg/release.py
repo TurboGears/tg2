c16a54eed81ee8a9dda67f3ab7a83fb1a4c0612c
mramm <none@none>
2008-07-29 19:55:11 +0000

    Bumping version post-release
    --HG--
    extra : convert_revision : svn%3A77541ad4-5f01-0410-9ede-a1b63cd9a898/trunk%405057

--- tg/release.py
+++ tg/release.py
@@ -1,5 +1,5 @@
 """TurboGears project related information"""
-version = "1.9.7a3"
+version = "1.9.7a4"
 description = "Next generation TurboGears built on Pylons"
 long_description="""
 TurboGears brings together a best of breed python tools
