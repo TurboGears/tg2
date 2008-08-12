f169a8830fcef81eb53df4104b96f5e7c89fde75
percious <none@none>
2008-08-12 05:50:14 +0000

    fixed tgapp to work with rumapp.  Some interesting manipulations for the import were required, code review is recommended, but all of the tests pass. enjoy.
    --HG--
    extra : convert_revision : svn%3A77541ad4-5f01-0410-9ede-a1b63cd9a898/trunk%405140

--- tg/wsgiapp.py
+++ tg/wsgiapp.py
@@ -1,26 +1,37 @@
 from pylons.wsgiapp import PylonsApp
+from pylons.util import class_name_from_module_name
+import sys
 
 class TGApp(PylonsApp):
-    
+
     def find_controller(self, controller):
         """Locates a controller by attempting to import it then grab
         the SomeController instance from the imported module.
-        
+
         Override this to change how the controller object is found once
         the URL has been resolved.
-        
+
         """
         # Check to see if we've cached the class instance for this name
         if controller in self.controller_classes:
             return self.controller_classes[controller]
-        
-        # Pull the controllers class name, import controller
-        full_module_name = self.config.paths.controller \
-            + controller.replace('/', '.')
-        
+
+        root_module_path = self.config['paths']['root']
+        controller_path = self.config['paths']['controllers']
+
+        #remove the part of the path we expect to be the root part (plus one '/')
+        assert controller_path.startswith(root_module_path)
+        controller_path = controller_path[len(root_module_path)+1:]
+
+        #attach the package
+        pylons_package = self.config['pylons.package']
+        full_module_name = pylons_package+'.'+controller_path.replace('/', '.')+'.'+controller.replace('/', '.')
+
+        print full_module_name
+
         # Hide the traceback here if the import fails (bad syntax and such)
         __traceback_hide__ = 'before_and_this'
-        
+
         __import__(full_module_name)
         module_name = controller.split('/')[-1]
         class_name = class_name_from_module_name(module_name) + 'Controller'
