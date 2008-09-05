8fb088d85da85c54f6c26c1e68473c29c149f40a
Gustavo <none@none>
2008-09-05 12:00:41 +0000

    Made optional the definition of the SA engine to be used for the tests. If not set, DBTest will use the one defined in test.ini.
    --HG--
    extra : convert_revision : svn%3A77541ad4-5f01-0410-9ede-a1b63cd9a898/trunk%405357

--- tg/testutil.py
+++ tg/testutil.py
@@ -6,18 +6,25 @@ from unittest import TestCase
 from sqlalchemy import create_engine
 from sqlalchemy.orm import clear_mappers
 
+from tg import config
+
 __all__ = ['DBTest']
 
 
 class DBTest(TestCase):
-    """The base class for testing models in TG2 Projects."""
+    """The base class for testing models in TG2 Projects.
+    
+    You should at least define C{model}. If you don't define C{database}, we'll
+    create an SQLAlchemy engine with the DB URI defined in test.ini.
+    
+    """
     model = None
     database = None
     
     def setUp(self):
         assert self.model != None, "Database test cases must define the model"
-        assert self.database != None, "Database test cases must define the "\
-                                      "database"
+        if self.database is None:
+            self.database = config['pylons.app_globals'].sa_engine
         self.model.init_model(self.database)
         self.model.metadata.create_all(self.database)
     
