b1ed2dac50d33c7301ed89b19f64e9be9b181f00
sanjiv <none@none>
2008-09-13 18:11:23 +0000

    imported postpone_commits to tg ns
    --HG--
    extra : convert_revision : svn%3A77541ad4-5f01-0410-9ede-a1b63cd9a898/trunk%405418

--- tg/__init__.py
+++ tg/__init__.py
@@ -53,7 +53,7 @@ assumptions about how things will work.
 from tg.wsgiapp import TGApp
 from tg.controllers import TGController, redirect, url, use_wsgi_app
 from tg.configuration import config
-from tg.decorators import validate, expose, override_template, paginate
+from tg.decorators import validate, expose, override_template, paginate, postpone_commits
 from tg.flash import flash, get_flash, get_status
 
 from pylons import g, request, response, tmpl_context, session
