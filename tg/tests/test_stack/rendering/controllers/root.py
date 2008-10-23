148bb291923f4ac351f95fd3cb0fdf1efa8db8b2
mramm <none@none>
2008-10-23 04:19:09 +0000

    project for renderig tests
    --HG--
    extra : convert_revision : svn%3A77541ad4-5f01-0410-9ede-a1b63cd9a898/trunk%405586

---
+++ tg/tests/test_stack/rendering/controllers/root.py
@@ -0,0 +1,33 @@
+"""Main Controller"""
+
+from tg import expose, redirect, config
+from tg.controllers import TGController
+
+class RootController(TGController):
+    @expose()
+    def index(self):
+        return "my foo"
+
+    @expose()
+    def config_test(self):
+        return str(config)
+    
+    @expose()
+    def config_attr_lookup(self):
+        return str(config.render_functions)
+    
+    @expose()
+    def config_dotted_values(self):
+        return str(config.pylons)
+    
+    @expose()
+    def config_attr_set(self, foo):
+        config.test_value = foo
+        return str(config.test_value)
+
+    @expose()
+    def config_dict_set(self, foo):
+        config['test_value'] = foo
+        return str(config.test_value)
+        
+
