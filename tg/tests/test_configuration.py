c4de7fd8b2a6ed9163eda2da88be8ce50d71d316
percious <none@none>
2009-01-11 06:00:51 +0000

    added a bunch of easy to add tests for configuration.
    --HG--
    extra : convert_revision : svn%3A77541ad4-5f01-0410-9ede-a1b63cd9a898/trunk%406058

--- tg/tests/test_configuration.py
+++ tg/tests/test_configuration.py
@@ -2,6 +2,7 @@
 Testing for TG2 Configuration
 """
 from nose.tools import eq_, raises
+import atexit
 
 from tg.configuration import AppConfig, config
 from tg.tests.base import TestWSGIController, make_app, setup_session_dir, teardown_session_dir, create_request
@@ -39,3 +40,84 @@ class TestPylonsConfigWrapper:
     @raises(AttributeError)
     def test_delattr_bad(self):
         del self.config.i_dont_exist
+
+class TestAppConfig:
+    def setup(self):
+        self.config = AppConfig()
+
+    def test_create(self):
+        pass
+
+    def test_setup_startup_and_shutdown_startup_callable(self):
+        def func():
+            a = 7
+        self.config.call_on_startup = [func]
+        self.config.setup_startup_and_shutdown()
+
+    def test_setup_startup_and_shutdown_callable_startup_with_exception(self):
+        def func():
+            raise Exception
+        self.config.call_on_startup = [func]
+        self.config.setup_startup_and_shutdown()
+
+    def test_setup_startup_and_shutdown_startup_not_callable(self):
+        self.config.call_on_startup = ['not callable']
+        self.config.setup_startup_and_shutdown()
+
+    def test_setup_startup_and_shutdown_shutdown_not_callable(self):
+        self.config.call_on_shutdown = ['not callable']
+        self.config.setup_startup_and_shutdown()
+
+    def test_setup_startup_and_shutdown_shutdown_callable(self):
+        def func():
+            a = 7
+        self.config.call_on_shutdown = [func]
+        self.config.setup_startup_and_shutdown()
+        assert (func, (), {}) in atexit._exithandlers
+
+    #this tests fails
+    def _test_setup_helpers_and_globals(self):
+        self.config.setup_helpers_and_globals()
+
+    def test_setup_sa_auth_backend(self):
+        self.config.setup_sa_auth_backend()
+
+    def test_setup_chameleon_genshi_renderer(self):
+        self.config.paths.templates = 'template_path'
+        self.config.setup_chameleongenshi_renderer()
+
+    def test_setup_genshi_renderer(self):
+        self.config.paths.templates = 'template_path'
+        self.config.setup_genshi_renderer()
+
+    def test_setup_jinja_renderer(self):
+        self.config.paths.templates = 'template_path'
+        self.config.setup_jinja_renderer()
+
+    def test_setup_mako_renderer(self):
+        self.config.paths.templates = 'template_path'
+        self.config.setup_mako_renderer()
+
+    def test_setup_sqlalchemy(self):
+        config['sqlalchemy.url'] = 'sqlite://'
+        class Package:
+            class model:
+                @classmethod
+                def init_model(package, engine):
+                    pass
+        self.config.package = Package()
+        self.config.setup_sqlalchemy()
+
+    def test_add_auth_middleware(self):
+        class Dummy:pass
+
+        self.config.sa_auth.dbsession = Dummy()
+        self.config.sa_auth.user_class = Dummy
+        self.config.sa_auth.group_class = Dummy
+        self.config.sa_auth.permission_class = Dummy
+        self.config.sa_auth.password_encryption_method = 'sha'
+
+        self.config.add_auth_middleware(None)
+
+    def test_add_static_file_middleware(self):
+        self.config.add_static_file_middleware(None)
