0f8fbecff91b13cd444b737693d4f509ddd5e889
percious <none@none>
2009-01-11 00:54:58 +0000

    added back in a few util tests
    --HG--
    extra : convert_revision : svn%3A77541ad4-5f01-0410-9ede-a1b63cd9a898/trunk%406052

--- tg/tests/test_util.py
+++ tg/tests/test_util.py
@@ -8,22 +8,22 @@ def setup():
     global path
     path = os.curdir
     os.chdir(os.path.abspath(os.path.dirname(tg.__file__)+'/..'))
-    
+
 def teardown():
     global path
     os.chdir(path)
 
-# These tests aren't reliable if the package in question has 
-# entry points.   
+# These tests aren't reliable if the package in question has
+# entry points.
 
-#def test_get_package_name():
-#    eq_(get_package_name(), 'tg')
-#    
-#def test_get_project_name():
-#    eq_(get_project_name(), 'TurboGears2')
-#    
-#def test_get_project_meta():
-#    eq_(get_project_meta('requires.txt'), 'TurboGears2.egg-info/requires.txt')
-#    
-#def test_get_model():
-#    eq_(get_model(), None)
+def test_get_package_name():
+    eq_(get_package_name(), 'tg')
+
+def test_get_project_name():
+    eq_(get_project_name(), 'TurboGears2')
+
+def test_get_project_meta():
+    eq_(get_project_meta('requires.txt'), 'TurboGears2.egg-info/requires.txt')
+
+def test_get_model():
+    eq_(get_model(), None)
