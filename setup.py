209810292091ae730d8f247baf9e90df16db7e0b
Gustavo <none@none>
2009-01-28 11:45:14 +0000

    Updated the dependencies on auth packages
    --HG--
    extra : convert_revision : svn%3A77541ad4-5f01-0410-9ede-a1b63cd9a898/trunk%406188

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ test_requirements = ['coverage',
                     'TurboJson',
                     'zope.sqlalchemy',
                     'SQLAlchemy>=0.5beta3',
-                    'repoze.what >= 1.0.2',
+                    'repoze.what >= 1.0.3',
                     'jinja',
                     'chameleon.genshi',
                     ]
@@ -44,7 +44,7 @@ setup(
         'WebFlash >= 0.1a7',
         'ToscaWidgets>=0.9',
         'repoze.who >= 1.0.10dev-r3295',
-        'repoze.what.plugins.sql > 1.0a2',
+        'repoze.what.plugins.sql > 1.0b1',
         'SQLAlchemy>=0.5beta3',
     ],
     extras_require={
