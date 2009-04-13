daee5b4ea64974775cfbd219eb99ee5ef4b42bd0
Christoph Zwerschke <cito@online.de>
2009-04-13 18:39:26 +0200

    Fix for trac ticket #2301 (hardcoded Unix path separator).

--- tg/tests/test_util.py
+++ tg/tests/test_util.py
@@ -7,7 +7,7 @@ path = None
 def setup():
     global path
     path = os.curdir
-    os.chdir(os.path.abspath(os.path.dirname(tg.__file__)+'/..'))
+    os.chdir(os.path.abspath(os.path.dirname(os.path.dirname(tg.__file__))))
 
 def teardown():
     global path
@@ -23,7 +23,7 @@ def test_get_project_name():
     eq_(get_project_name(), 'TurboGears2')
 
 def test_get_project_meta():
-    eq_(get_project_meta('requires.txt'), 'TurboGears2.egg-info/requires.txt')
+    eq_(get_project_meta('requires.txt'), os.path.join('TurboGears2.egg-info', 'requires.txt'))
 
 def test_get_model():
     eq_(get_model(), None)
