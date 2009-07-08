614ac195f05a6082f4a19d2e3b3d5b8f219caecd
Christoph Zwerschke <cito@online.de>
2009-07-08 23:27:49 +0200

    Translations did not work in the 2.1 branch any more because setup_i18n() was not called. Introduced "i18n_enable" config setting to allow suppressing this i18n setup if you don't need it.

--- tg/controllers/dispatcher.py
+++ tg/controllers/dispatcher.py
@@ -6,7 +6,7 @@ Start at the RootController, the root controller must
 have a _dispatch function, which defines how we move
 from object to object in the system.
 Continue following the dispatch mechanism for a given
-controller until you reach another controller with a 
+controller until you reach another controller with a
 _dispatch method defined.  Use the new _dispatch
 method until anther controller with _dispatch defined
 or until the url has been traversed to entirety.
@@ -22,9 +22,11 @@ import pylons
 import mimetypes
 from pylons.controllers import WSGIController
 from tg.exceptions import HTTPNotFound
+from tg.i18n import setup_i18n
 from tg.util import odict
 
 HTTPNotFound = HTTPNotFound().exception
+
 
 class DispatchState(object):
     """
@@ -43,30 +45,34 @@ class DispatchState(object):
         self.dispatcher = None
 
     def add_controller(self, location, controller):
-        """Adds a controller object to the stack"""
+        """Add a controller object to the stack"""
         self.controller_path[location] = controller
 
     def add_method(self, method, remainder):
-        """Adds the final method that will be called in the _call method"""
+        """Add the final method that will be called in the _call method"""
         self.method = method
         self.remainder = remainder
 
     def add_routing_args(self, current_path, remainder, fixed_args, var_args):
-        """adds the "intermediate" routing args for a given controller mounted at the current_path"""
+        """
+        Add the "intermediate" routing args for a given controller mounted
+        at the current_path
+        """
         i = 0
         for i, arg in enumerate(fixed_args):
-            if i>=len(remainder):
-                break;
+            if i >= len(remainder):
+                break
             self.routing_args[arg] = remainder[i]
         remainder = remainder[i:]
         if var_args and remainder:
             self.routing_args[current_path] = remainder
-        
+
     @property
     def controller(self):
         """returns the current controller"""
         return self.controller_path.getitem(-1)
-    
+
+
 class Dispatcher(WSGIController):
     """
        Extend this class to define your own mechanism for dispatch.
@@ -74,23 +80,23 @@ class Dispatcher(WSGIController):
 
     def _call(self, controller, params, remainder=None):
         """
-            Override This function to define how your controller method should be called.
+        Override this function to define how your controller method should be called.
         """
         response = controller(*remainder, **dict(params))
         return response
-    
+
     def _get_argspec(self, func):
         try:
             cached_argspecs = self.__class__._cached_argspecs
         except AttributeError:
             self.__class__._cached_argspecs = cached_argspecs = {}
-        
+
         try:
             argspec = cached_argspecs[func.im_func]
         except KeyError:
             argspec = cached_argspecs[func.im_func] = getargspec(func)
         return argspec
- 
+
     def _get_params_with_argspec(self, func, params, remainder):
         params = params.copy()
         argspec = self._get_argspec(func)
@@ -98,25 +104,25 @@ class Dispatcher(WSGIController):
         if argvars and enumerate(remainder):
             for i, var in enumerate(argvars):
                 if i >= len(remainder):
-                    break;
+                    break
                 params[var] = remainder[i]
         return params
-    
+
     def _remove_argspec_params_from_params(self, func, params, remainder):
         """Remove parameters from the argument list that are
            not named parameters
            Returns: params, remainder"""
-        
+
         # figure out which of the vars in the argspec are required
         argspec = self._get_argspec(func)
         argvars = argspec[0][1:]
-            
+
         # if there are no required variables, or the remainder is none, we
         # have nothing to do
         if not argvars or not remainder:
             return params, remainder
-        
-        #this is a work around for a crappy api choice in getargspec
+
+        # this is a work around for a crappy api choice in getargspec
         argvals = argspec[3]
         if argvals is None:
             argvals = []
@@ -127,28 +133,28 @@ class Dispatcher(WSGIController):
             required_vars = argvars[:len(argvals)-1]
             optional_vars = argvars[len(argvals)-1:]
 
-        #make a copy of the params so that we don't modify the existing one
+        # make a copy of the params so that we don't modify the existing one
         params=params.copy()
-        
-        # replace the existing required variables with the values that come in from params
-        # these could be the parameters that come off of validation.
+
+        # replace the existing required variables with the values that come in
+        # from params these could be the parameters that come off of validation.
         for i,var in enumerate(required_vars):
             remainder[i] = params[var]
             del params[var]
-            
+
         #remove the optional vars from the params until we run out of remainder
         for var in optional_vars:
             if var in params:
                 del params[var]
 
         return params, remainder
-    
+
     def _dispatch(self, state, remainder):
         """override this to define how your controller should dispatch.
         returns: dispatcher, controller_path, remainder
         """
         raise NotImplementedError
-    
+
     def _get_dispatchable(self, url_path):
         """
         Returns a tuple (controller, remainder, params)
@@ -157,7 +163,7 @@ class Dispatcher(WSGIController):
           url
             url as string
         """
-        
+
         pylons.request.response_type = None
         pylons.request.response_ext = None
         if url_path and '.' in url_path[-1]:
@@ -176,21 +182,26 @@ class Dispatcher(WSGIController):
         state.add_controller('/', self)
         state.dispatcher = self
         state =  state.controller._dispatch(state, url_path)
-        
-        pylons.tmpl_context.controller_url = '/'.join(url_path[:-len(state.remainder)])
-        
+
+        pylons.tmpl_context.controller_url = '/'.join(
+            url_path[:-len(state.remainder)])
+
 
         state.routing_args.update(params)
         if hasattr(state.dispatcher, '_setup_wsgiorg_routing_args'):
-            state.dispatcher._setup_wsgiorg_routing_args(url_path, state.remainder, state.routing_args)
+            state.dispatcher._setup_wsgiorg_routing_args(
+                url_path, state.remainder, state.routing_args)
 
         return state.method, state.controller, state.remainder, params
 
     def _setup_wsgiorg_routing_args(self, url_path, remainder, params):
-        pass
-        #this needs to get added back in after we understand why it breaks pagination.
-#        pylons.request.environ['wsgiorg.routing_args'] = (tuple(remainder), params)
-    
+        """
+        This is expected to be overridden by any subclass that wants to set
+        the routing_args (RestController). Do not delete.
+        """
+        # this needs to get added back in after we understand why it breaks pagination:
+        # pylons.request.environ['wsgiorg.routing_args'] = (tuple(remainder), params)
+
     def _setup_wsgi_script_name(self, url_path, remainder, params):
         pass
 
@@ -198,8 +209,8 @@ class Dispatcher(WSGIController):
         """
         This function is called from within Pylons and should not be overidden.
         """
-        func_name = func.__name__
-        pylons.request.path.split('/')[1:]
+        if pylons.config.get('i18n_enabled', True):
+            setup_i18n()
 
         url_path = pylons.request.path.split('/')[1:]
 
@@ -209,33 +220,32 @@ class Dispatcher(WSGIController):
         func, controller, remainder, params = self._get_dispatchable(url_path)
 
         if hasattr(controller, '__before__') and not hasattr(controller, '_before'):
-            warn("this functionality is going to removed in the next minor version,"\
-                 " please use _before instead."
-                 )
+            warn("this functionality is going to removed in the next minor version,"
+                 " please use _before instead.")
             print controller
             controller.__before__(*args)
         if hasattr(controller, '_before'):
             controller._before(*args)
-            
+
         self._setup_wsgi_script_name(url_path, remainder, params)
 
         r = self._call(func, params, remainder=remainder)
 
         if hasattr(controller, '__after__'):
-            warn("this functionality is going to removed in the next minor version,"\
-                 " please use _after instead."
-                 )
+            warn("this functionality is going to removed in the next minor version,"
+                 " please use _after instead.")
             controller.__after__(*args)
         if hasattr(controller, '_after'):
             controller._after(*args)
         return r
-    
+
     def routes_placeholder(self, url='/', start_response=None, **kwargs):
         """
         This function does not do anything.  It is a placeholder that allows
         Routes to accept this controller as a target for its routing.
         """
         pass
+
 
 class ObjectDispatcher(Dispatcher):
     """
@@ -279,21 +289,21 @@ class ObjectDispatcher(Dispatcher):
         for method in methods:
             if self._is_exposed(controller, method):
                 return getattr(controller, method)
-    
+
     def _is_exposed(self, controller, name):
         """Override this function to define how a controller method is
         determined to be exposed.
-        
+
         :Arguments:
           controller - controller with methods that may or may not be exposed.
           name - name of the method that is tested.
-        
+
         :Returns:
            True or None
         """
         if hasattr(controller, name) and ismethod(getattr(controller, name)):
             return True
-        
+
     def _is_controller(self, controller, name):
         """
         Override this function to define how an object is determined to be a
@@ -304,18 +314,17 @@ class ObjectDispatcher(Dispatcher):
     def _dispatch_controller(self, current_path, controller, state, remainder):
         """
            Essentially, this method defines what to do when we move to the next
-           layer in the url chain, if a new controller is needed.  If the new
-           controller has a _dispatch method, dispatch proceeds to the new controller's
-           mechanism.
-           
-           Also, this is the place where the controller is checked for controller-level
-           security.
+           layer in the url chain, if a new controller is needed.
+           If the new controller has a _dispatch method, dispatch proceeds to
+           the new controller's mechanism.
+
+           Also, this is the place where the controller is checked for
+           controller-level security.
         """
         if hasattr(controller, '_dispatch'):
             if isclass(controller):
-                warn("this functionality is going to removed in the next minor version,"\
-                     " please create an instance of your sub-controller instead"
-                     )
+                warn("this functionality is going to removed in the next minor version,"
+                     " please create an instance of your sub-controller instead")
                 controller = controller()
             if hasattr(controller, "im_self"):
                 obj = controller.im_self
@@ -329,12 +338,12 @@ class ObjectDispatcher(Dispatcher):
             return controller._dispatch(state, remainder)
         state.add_controller(current_path, controller)
         return self._dispatch(state, remainder)
-        
+
     def _dispatch_first_found_default_or_lookup(self, state, remainder):
         """
-           When the dispatch has reached the end of the tree but not found an applicable method, 
-           so therefore we head back up the branches of the tree until we found a method which
-           matches with a default or lookup method.
+        When the dispatch has reached the end of the tree but not found an
+        applicable method, so therefore we head back up the branches of the
+        tree until we found a method which matches with a default or lookup method.
         """
         orig_url_path = state.url_path
         if len(remainder):
@@ -348,11 +357,12 @@ class ObjectDispatcher(Dispatcher):
             if self._is_exposed(controller, 'lookup'):
                 controller, remainder = controller.lookup(*remainder)
                 state.url_path = orig_url_path
-                return self._dispatch_controller('lookup', controller, state, remainder)
+                return self._dispatch_controller(
+                    'lookup', controller, state, remainder)
             state.controller_path.pop()
             if len(state.url_path):
                 remainder = list(remainder)
-                remainder.insert(0,state.url_path[-1])
+                remainder.insert(0, state.url_path[-1])
                 state.url_path.pop()
         raise HTTPNotFound
 
@@ -378,18 +388,21 @@ class ObjectDispatcher(Dispatcher):
 
         #an exposed method matching the path is found
         if self._is_exposed(current_controller, current_path):
-            state.add_method(getattr(current_controller, current_path), remainder[1:])
+            state.add_method(getattr(
+                current_controller, current_path), remainder[1:])
             return state
-        
+
         #another controller is found
         if hasattr(current_controller, current_path):
             current_controller = getattr(current_controller, current_path)
-            return self._dispatch_controller(current_path, current_controller, state, remainder[1:])
-        
+            return self._dispatch_controller(
+                current_path, current_controller, state, remainder[1:])
+
         #dispatch not found
         return self._dispatch_first_found_default_or_lookup(state, remainder)
 
     def _setup_wsgiorg_routing_args(self, url_path, remainder, params):
-        """This is expected to be overridden by any subclass that wants to set the routing_args (RestController)
-           Do not delete.
+        """
+        This is expected to be overridden by any subclass that wants to set
+        the routing_args (RestController). Do not delete.
         """
