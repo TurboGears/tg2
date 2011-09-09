8e1a8b54fedb78137be49f8056143d33e80b0392
Alessandro Molina <alessandro.molina@axant.it>
2011-09-10 00:13:49 +0200

    Add test for lurl to verify gives the same results as url

--- tests/test_controllers.py
+++ tests/test_controllers.py
@@ -84,6 +84,10 @@ def test_url_doesnt_change_tgparams():
     result = url('/foo', params, spamm='ham')
     eq_(params['spamm'], 'eggs')
 
+def test_lurl():
+    params = {'spamm':'eggs', 'hamm':None }
+    assert url('/foo', params=params) == str(lurl('/foo', params=params))
+
 #def test_approotsWithPath():
 #    create_request('/coolsite/root/subthing/', {'SCRIPT_NAME' : '/subthing'})
 #    pylons.config.update({"server.webpath":"/coolsite/root"})
