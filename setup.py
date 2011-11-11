de739e7111654a5eabc97af8fd3ba58f3b75777a
Alessandro Molina <alessandro.molina@axant.it>
2011-11-11 02:38:10 +0100

    Require specific webob version as in various cases webob1.2 has been installed ignoring package index restrictions

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'WebOb >= 1.0.7',
+        'WebOb == 1.0.8',
         'Pylons >= 0.9.7',
         'WebFlash >= 0.1a8',
         'WebError >= 0.10.1',
