eb002c35438528207b796de77663ab1d054ad793
Alessandro Molina <alessandro.molina@axant.it>
2012-11-01 00:14:01 +0100

    Always update even with nonzero is not available

--- tg/configuration/sqla/auth.py
+++ tg/configuration/sqla/auth.py
@@ -3,6 +3,6 @@ from repoze.who.plugins.sa import SQLAlchemyAuthenticatorPlugin
 
 def create_default_authenticator(user_class, dbsession, translations=None, **unused):
     sqlauth = SQLAlchemyAuthenticatorPlugin(user_class, dbsession)
-    if translations:
+    if translations is not None:
         sqlauth.translations.update(translations)
     return unused, sqlauth
