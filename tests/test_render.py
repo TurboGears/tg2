67ec56479765aba6052565ee24e41546967ce131
Alessandro Molina <alessandro.molina@axant.it>
2012-11-21 21:48:26 +0100

    Bring to full coverage jinja_lookup and kajiki_lookup

--- tests/test_render.py
+++ tests/test_render.py
@@ -1,17 +1,47 @@
 """
 Testing for TG2 Configuration
 """
-from nose.tools import eq_, raises
-import atexit
+from nose.tools import raises
+from nose import SkipTest
 
-from tg.render import render, MissingRendererError
+import tg
+from tg.render import MissingRendererError, render_jinja
 from tests.base import TestWSGIController, make_app, setup_session_dir, teardown_session_dir, create_request
+
+from tg.configuration import AppConfig
+from tg._compat import PY3
 
 def setup():
     setup_session_dir()
 def teardown():
     teardown_session_dir()
 
+class FakePackage:
+    __name__ = 'tests'
+    __file__ = __file__
+
+    class lib:
+        class app_globals:
+            class Globals:
+                pass
+
 @raises(MissingRendererError)
 def test_render_missing_renderer():
-    render({}, 'gensh')
+    conf = AppConfig(minimal=True)
+    app = conf.make_wsgi_app()
+
+    tg.render_template({}, 'gensh')
+
+def test_jinja_lookup_nonexisting_template():
+    conf = AppConfig(minimal=True)
+    conf.renderers.append('jinja')
+    conf.package = FakePackage
+    app = conf.make_wsgi_app()
+
+    from jinja2 import TemplateNotFound
+    try:
+        render_jinja('tg.this_template_does_not_exists', {'app_globals':tg.config['tg.app_globals']})
+        assert False
+    except TemplateNotFound:
+        pass
+
