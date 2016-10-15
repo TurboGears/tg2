94c7b356ea556a94e2275e8e3c6aa573ed06bec7
Alessandro Molina <alessandro.molina@axant.it>
2016-10-15 15:37:50 +0200

    oops, fix test on py3

--- tests/test_tg_controller_dispatch.py
+++ tests/test_tg_controller_dispatch.py
@@ -370,7 +370,7 @@ class BasicTGController(TGController):
 
     @expose()
     def response_responded(self):
-        tg.response.body = 'Body Response'
+        tg.response.text = 'Body Response'
         tg.response.content_type = 'text/plain'
         return tg.response
 
