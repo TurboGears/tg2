234719622b411b1a816e9b4ac18adad305a47b4a
Alessandro Molina <alessandro.molina@axant.it>
2018-01-29 21:11:40 +0100

    Update Errorware tests

--- tests/test_errorware.py
+++ tests/test_errorware.py
@@ -1,6 +1,5 @@
-from tg.error import ErrorReporter
-from tg.error import SlowReqsReporter
-
+from tg.configuration.configurator.steps.error_reporting import ErrorReportingConfigurationStep
+from tg.configuration.configurator.steps.slow_requests import SlowRequestsConfigurationStep
 
 def simple_app(environ, start_response):
     status = '200 OK'
@@ -13,37 +12,43 @@ class TestErrorReporterConfig(object):
 
     middleware_name = 'TraceErrorsMiddleware'
 
+    def _make(self, **options):
+        step = ErrorReportingConfigurationStep()
+        conf = {'trace_errors.%s' % k: v for k, v in options.items() if k != 'debug'}
+        if 'debug' in options:
+            conf['debug'] = options['debug']
+        step._configure_backlash(conf, None)
+        return step._add_middleware(conf, simple_app)
+
     def test_enable_none(self):
-        app = ErrorReporter(simple_app, {})
+        app = self._make()
         assert app.__class__.__name__ == self.middleware_name
         assert not app.reporters
 
     def test_enable_false(self):
-        app = ErrorReporter(simple_app, {}, enable=False)
+        app = self._make(enable=False)
         assert app.__class__.__name__ != self.middleware_name
 
     def test_enable_true(self):
-        app = ErrorReporter(simple_app, {}, enable=True)
+        app = self._make(enable=True)
         assert app.__class__.__name__ == self.middleware_name
         assert not app.reporters
 
     def test_enable_email(self):
-        app = ErrorReporter(simple_app, {},
-            error_email='user@somedomain.com')
+        app = self._make(error_email='user@somedomain.com')
         assert app.__class__.__name__ == self.middleware_name
         assert any(r.__class__.__name__ == 'EmailReporter'
             for r in app.reporters)
 
     def test_enable_sentry(self):
-        app = ErrorReporter(simple_app, {},
-            sentry_dsn='http://public:secret@example.com/1')
+        app = self._make(sentry_dsn='http://public:secret@example.com/1')
         assert app.__class__.__name__ == self.middleware_name
         assert any(r.__class__.__name__ == 'SentryReporter'
             for r in app.reporters)
 
     def test_debug_mode(self):
-        app = ErrorReporter(simple_app, dict(debug='on'), enable=True,
-            error_email='user@somedomain.com')
+        app = self._make(debug='on', enable=True,
+                         error_email='user@somedomain.com')
         assert app is simple_app
 
 
@@ -51,30 +56,36 @@ class TestSlowReqsReporterConfig(object):
 
     middleware_name = 'TraceSlowRequestsMiddleware'
 
+    def _make(self, **options):
+        step = SlowRequestsConfigurationStep()
+        conf = {'trace_slowreqs.%s' % k: v for k, v in options.items() if k != 'debug'}
+        if 'debug' in options:
+            conf['debug'] = options['debug']
+        step._configure_backlash(conf, None)
+        return step._add_middleware(conf, simple_app)
+
     def test_disable_all(self):
-        app = SlowReqsReporter(simple_app, {})
+        app = self._make()
         assert app is simple_app
 
     def test_enable_without_reporter(self):
-        app = SlowReqsReporter(simple_app, {}, enable=True)
+        app = self._make(enable=True)
         assert app.__class__.__name__ == self.middleware_name
         assert not app.reporters
 
     def test_enable_email(self):
-        app = SlowReqsReporter(simple_app, {}, enable=True,
-            error_email='user@somedomain.com')
+        app = self._make(enable=True, error_email='user@somedomain.com')
         assert app.__class__.__name__ == self.middleware_name
         assert any(r.__class__.__name__ == 'EmailReporter'
             for r in app.reporters)
 
     def test_enable_sentry(self):
-        app = SlowReqsReporter(simple_app, {}, enable=True,
-            sentry_dsn='http://public:secret@example.com/1')
+        app = self._make(enable=True, sentry_dsn='http://public:secret@example.com/1')
         assert app.__class__.__name__ == self.middleware_name
         assert any(r.__class__.__name__ == 'SentryReporter'
             for r in app.reporters)
 
     def test_debug_mode(self):
-        app = SlowReqsReporter(simple_app, dict(debug='on'), enable=True,
-            error_email='user@somedomain.com')
+        app = self._make(debug='on', enable=True,
+                         error_email='user@somedomain.com')
         assert app is simple_app
