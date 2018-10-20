bb7e243a14f53cd7ef2fe2b90517c63a0b907d86
Alessandro Molina <amol@turbogears.org>
2018-10-20 18:34:02 +0200

    Some tweaks

--- tg/configurator/__init__.py
+++ tg/configurator/__init__.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from .base import Configurator
+from .base import Configurator, ConfigurationComponent
+from .base import (BeforeConfigConfigurationAction,
+                   ConfigReadyConfigurationAction,
+                   AppReadyConfigurationAction,
+                   EnvironmentLoadedConfigurationAction)
 from .application import ApplicationConfigurator
 from .minimal import MinimalApplicationConfigurator
 from .fullstack import FullStackApplicationConfigurator
